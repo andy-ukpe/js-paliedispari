16c66759cc245926923f5d6b83cdcbd800deb4af
andy <andy.ukpe@outlook.com>
2020-05-29 16:06:12 +0200

    stesura esercizio pari e dispari

--- script.js
+++ script.js
@@ -6,12 +6,40 @@
 // Stabiliamo se la somma dei due numeri è pari
 // o dispari (usando una funzione)
 
+// UTENTE SCEGLIE SE PARI O DISPARI
+var isOddUser = prompt('Scegli se pari o dispari');
+ciclo di controllo input
+while( (isOddUser == ' ') || () ){
+  prompt('Errore! inserire pari o dispari');
+}
+var pari = false;
+if(isOddUser == "pari"){
+  var pari = true;
+}
+
+// UTENTE INSERISCE NUMERO DA 1 A 5
+var numeroUser = prompt('Scegli un numero da 1 a 5');
+ciclo di controllo input
+while( isNaN(numeroUser) || (numeroUser < 1 || numeroUser > 5 ) ){
+  prompt('Errore! inserire 1 o 5');
+}
+console.log(numeroUser);
+
 
 // generare numero random per Pc
 function generareNumPc(numeroMassimo){
-  var risultato = Math.floor(Math.random() * numeroMassimo ) + 1;
-  return risultato;
+  var numeroRandom = Math.floor(Math.random() * numeroMassimo ) + 1;
+  return numeroRandom;
 }
 
 var numeroPc = generareNumPc(5);
 console.log(numeroPc);
+
+//SOMMIAMO I NUMERI
+var somma = numeroPc + numeroUser;
+
+// stabilire se la somma dei due numeri è pari o dispari
+function isOddorEver(){
+  var modulo = (somma % 2 == 0);
+  return modulo
+}
