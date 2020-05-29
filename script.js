3e92df7cafccd186b7de84bf320546ae8c389ea8
andy <andy.ukpe@outlook.com>
2020-05-29 20:58:56 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5)
-// per il computer (usando una funzione).
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari
-// o dispari (usando una funzione)
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 // UTENTE SCEGLIE SE PARI O DISPARI
-var isOddUser = prompt('Scegli se pari o dispari');
-ciclo di controllo input
-while( (isOddUser == ' ') || () ){
-  prompt('Errore! inserire pari o dispari');
-}
-var pari = false;
-if(isOddUser == "pari"){
-  var pari = true;
-}
+  var isOddUser = prompt('Scegli se pari o dispari');
+    isOddUser = isOddUser.toLowerCase();
+// ciclo di controllo input
+  while( (isOddUser == ' ') || (isOddUser != 'pari' && isOddUser != 'dispari') ){
+    isOddUser = prompt('Errore! inserire pari o dispari');
+  }
+// verifico che l'input pari lo sia oppure sia dispari
+   var pari;
+
+   while (pari != true && pari != false) {
+     if(isOddUser === "pari"){
+      pari = true;
+    } else{
+      pari = false;
+    }
+  }
+    console.log( 'condizione di verità: ' + pari);
+   console.log(isOddUser);
 
 // UTENTE INSERISCE NUMERO DA 1 A 5
-var numeroUser = prompt('Scegli un numero da 1 a 5');
-ciclo di controllo input
-while( isNaN(numeroUser) || (numeroUser < 1 || numeroUser > 5 ) ){
-  prompt('Errore! inserire 1 o 5');
+  var numeroUser = prompt('Scegli un numero da 1 a 5');
+// ciclo di controllo input
+  while( isNaN(numeroUser) || (numeroUser < 1 || numeroUser > 5 ) ){
+    numeroUser = prompt('Errore! inserire 1 o 5');
 }
-console.log(numeroUser);
-
-
-// generare numero random per Pc
-function generareNumPc(numeroMassimo){
+// GENERARE IL NUMERO PER IL PC
+  function generareNumPc(numeroMassimo){
   var numeroRandom = Math.floor(Math.random() * numeroMassimo ) + 1;
   return numeroRandom;
 }
 
-var numeroPc = generareNumPc(5);
-console.log(numeroPc);
+  var numeroPc = generareNumPc(5);
+  console.log('numero pc: ' + numeroPc);
 
-//SOMMIAMO I NUMERI
-var somma = numeroPc + numeroUser;
+// SOMMARE I NUMERI
+  var somma = numeroPc + parseInt(numeroUser);
+    console.log('questa è la somma: ' + somma);
 
-// stabilire se la somma dei due numeri è pari o dispari
-function isOddorEver(){
-  var modulo = (somma % 2 == 0);
-  return modulo
+// STABILIRE SE LA SOMMA DEI DUE NUMERI è PARI O DISPARI
+
+  var sommaDaAnalizzare = verificareSePariODispari(somma);
+
+function verificareSePariODispari(sommaDaAnalizzare){
+  var risultato = false;
+  if( somma % 2 == 0 ){
+    risultato = true;
+  }
+  return risultato;
+}
+if(sommaDaAnalizzare === true && pari === true ) {
+  console.log('Hai vinto! la somma è pari' + ' ' + somma);
+}
+else if(sommaDaAnalizzare === false && pari === false) {
+  console.log('Hai vinto! la somma è dispari' + ' ' + somma);
+}
+else if (sommaDaAnalizzare === true && pari === false) {
+  console.log('Hai perso! la somma è pari' + ' ' + somma);
+}
+else {
+  console.log('Hai perso! la somma è dispari' + ' ' + somma);
 }
